3e69f8412b8acff25f1378a2843c5b587cbc03da
Nguyen Van Gia Thinh <thinh.nguyen@pn.com.vn>
2019-11-18 17:52:48 +0700

    Student

---
+++ recyclerStudentView/app/src/main/java/com/example/recyclerstudentview/Student.java
@@ -0,0 +1,12 @@
+package com.example.recyclerstudentview;
+
+class Student {
+
+    String nameS;
+    int score;
+
+    public Student(String name, int score) {
+        this.nameS = nameS;
+        this.score = score;
+    }
+}
